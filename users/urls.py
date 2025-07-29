25ef77a54b5bd1003716231f3d0a32af32e1f41f
loowpts <pavelbariik@yandex.ru>
2025-07-29 11:09:05 +0300

    added password reset by email

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView, LoginView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import (
+    LogoutView, LoginView,
+    PasswordResetView, PasswordResetDoneView,
+    PasswordResetConfirmView, PasswordResetCompleteView
+)
 from . import views
 
 app_name = 'users'
@@ -10,5 +15,23 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='users:login'), name='logout'),
     path('activate/<str:username>/<str:token>/', views.activate_account, name='activate'),
+    
+    path('password_reset/', PasswordResetView.as_view(
+        template_name='users/password_reset.html',
+        email_template_name='users/registration/password_reset_email.html',
+        success_url=reverse_lazy('users:password_reset_done'),  # Добавь эту строку
+    ), name='password_reset'),
+    
+    path('password_reset/done/', PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'
+    ), name='password_reset_done'),
+    
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html',
+        success_url=reverse_lazy('users:password_reset_complete')
+    ), name='password_reset_confirm'),
+    
+    path('reset/done/', PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
-

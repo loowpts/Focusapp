9d085ce9e011ff9be787eb42e6dbae9bbec3aca3
lee. <pavelbariik@yandex.ru>
2025-11-30 22:22:02 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -2,12 +2,11 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-
-
 
 DEBUG = True
 
